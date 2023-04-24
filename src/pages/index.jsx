d096837c6586f60d09cfce393f552446c02f3b87
Ajay Bhuj <bhujajay9@gmail.com>
2023-04-24 13:07:33 +0530

    cnfig changed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,19 +4,7 @@ import Navbar from "../../components/Navbar/Navbar";
 const page = () => {
   return (
     <div>
-      <header>
-        <img src="./assests/logo.png" alt="SKIT logo" />
-
-        <nav className="flex items-center justify-between bg-primary py-3">
-          <ul className="flex items-center flex-1 justify-around text-white">
-            <NavbarItem link={"/"} title={"Home"} />
-            <NavbarItem link="/alumni-directory" title={"Alumni Directory"} />
-            <NavbarItem link="/activities" title={"Activities"} />
-            <NavbarItem link="/aboutus" title={"About us"} />
-            <NavbarItem link="/login" title={"Login"} />
-          </ul>
-        </nav>
-      </header>
+      <Navbar />
       <div className="hero-container w-screen h-screen bg-gradient-to-r from-cyan-500 to-blue-500 ">
         <div className="flex justify-center items-center h-full text-white">
           <h5 className="text-2xl font-bold">
@@ -29,16 +17,16 @@ const page = () => {
           Our Purpose of Alumni Association
         </p>
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-3 w-50 mx-auto justify-center gap-4  py-4 px-8 md:px-14 lg:px-24  cursor-pointer 	">
-        <Purpose
+      <div className="grid grid-cols-1 md:grid-cols-3  mx-auto justify-center gap-4  py-4 px-8 md:px-14 lg:px-24  cursor-pointer 	">
+        <PurposeCard
           text="Our vision is a worldwide alumni network that fosters lifelong connections and engagement with the college, empowering alumni to make a positive impact in their lives and the community."
           icon="./assests/icons/vision.png"
         />
-        <Purpose
+        <PurposeCard
           text=" We aim to foster a spirit of lifelong learning and professional development, and to support the college's mission of promoting academic excellence, diversity, and social responsibility."
           icon="./assests/icons/mission.png"
         />
-        <Purpose
+        <PurposeCard
           text=" Our objectives are to increase alumni engagement through diverse
             programs and events, provide networking resources for career growth,
             and support the college's strategic initiatives through fundraising
@@ -46,25 +34,33 @@ const page = () => {
           icon="./assests/icons/objectives.png"
         />
       </div>
+      <div className="flex justify-center items-center py-16 h-full text-black pb-6">
+        <h5 className="text-3xl font-semibold">
+          Join the Alumni Association: Register Today!
+        </h5>
+      </div>
+      <div className="flex place-items-center  flex-auto justify-items-center gap-10 place-content-center ">
+        <div className="shrink ">
+          <img className="" src="./assests/registeration.png" alt="" />
+        </div>
+        <div className="w-80">
+          <p className="text-center">
+            Welcome to the alumni community of SKIT. As a member of the alumni
+            association, you will have access to a wide range of benefits,
+            including networking opportunities, career resources, and exclusive
+            events.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default page;
-export const NavbarItem = ({ title, link }) => {
-  return (
-    <li
-      href={link}
-      className=" cursor-pointer mx-4 py-3 px-4 hover:opacity-90 hover:bg-green-600 rounded-md"
-    >
-      {title}
-    </li>
-  );
-};
 
-const Purpose = ({ text, icon }) => {
+const PurposeCard = ({ text, icon }) => {
   return (
-    <div className="border-primary border-2 hover:border-blue-500 bg-white p-4 rounded-xl">
+    <div Name="border-primary border-2 hover:border-blue-500 bg-white p-4 rounded-xl">
       <div className="text-center mb-2">
         <img src={icon} alt="Icon" className="w-8 h-8 mx-auto" />
       </div>
